2232cbe1e8636a022325aaa7c4b7465ddf20b8c8
Sonya Moisset <sonya.moisset@gmail.com>
2016-12-12 09:30:55 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const PANELS = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('.open-active';)
+  }
+}
+
+PANELS.forEach(panel => panel.addEventListener('click', toggleOpen));
+PANELS.forEach(panel => panel.addEventListener('transitionend', toggleActive));
